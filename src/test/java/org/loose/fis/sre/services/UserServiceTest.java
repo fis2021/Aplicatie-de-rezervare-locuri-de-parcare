9bc7a6f324343c49b20583294cdbd982f7d91f37
Augustin Rosca <augustin.rosca@student.upt.ro>
2021-05-12 15:22:10 +0300

    ADRLP-57 Am facut testarea pentru adaugare

--- src/test/java/org/loose/fis/sre/services/UserServiceTest.java
+++ src/test/java/org/loose/fis/sre/services/UserServiceTest.java
@@ -1,7 +1,9 @@
 package org.loose.fis.sre.services;
 
+import org.dizitart.no2.exceptions.InvalidIdException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.loose.fis.sre.controllers.MainStageController;
 import org.loose.fis.sre.controllers.RegisterController;
 import org.loose.fis.sre.exceptions.LoginExceptii;
 import org.loose.fis.sre.exceptions.RegistrationExceptii;
@@ -37,6 +39,34 @@ class UserServiceTest {
             assertFalse(UserService.checkAccountInformations("test","test"));
         });
     }
-
-
+    @Test
+    @DisplayName("Adaugare Dublicate")
+    void addMallDuplicateTest(){
+        assertThrows(UsernameAlreadyExistsException.class, () -> {
+            MallService.initDatabaseMall();
+            MallService.addMall("Petrea","Papagalul","2");
+            MallService.addMall("Petrea","Papagalul","2");
+        });
+    }
+    @Test
+    @DisplayName("Adaugare mall Username field gol")
+    void addMallEmptyUsername(){
+        assertThrows(InvalidIdException.class, () -> {
+            MallService.addMall("","Papagalul","2");
+        });
+    }
+    @Test
+    @DisplayName("Adaugare mall Password field gol")
+    void addMallEmptyPassword(){
+        assertThrows(NullPointerException.class, () -> {
+            MallService.addMall("Petrea","","2");
+        });
+    }
+    @Test
+    @DisplayName("Adaugare mall Floor field gol")
+    void addMallEmptyFloor(){
+        assertThrows(UsernameAlreadyExistsException.class, () -> {
+            MallService.addMall("Petrea","Papagalul","");
+        });
+    }
 }
