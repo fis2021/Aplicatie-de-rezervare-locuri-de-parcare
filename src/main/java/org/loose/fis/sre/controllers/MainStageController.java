3164659d19b408975e0b665ff85f92e0dacb6d0d
Augustin Rosca <augustin.rosca@student.upt.ro>
2021-05-12 14:58:48 +0300

    ADRLP-26 Am facut interfata si functionabilitatea pentru delete mall.

--- src/main/java/org/loose/fis/sre/controllers/MainStageController.java
+++ src/main/java/org/loose/fis/sre/controllers/MainStageController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -20,11 +21,15 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import static org.loose.fis.sre.services.MallService.checkMallDoesNotAlreadyExist;
 
 public class MainStageController implements  Initializable{
 
     @FXML
     private VBox VboxTable;
+
+    @FXML
+    private VBox VboxAddMall;
 
     @FXML
     private TableView<Mall> Table = new TableView<>();
@@ -39,9 +44,6 @@ public class MainStageController implements  Initializable{
     private TableColumn<Mall, String> TableFloors;
 
     @FXML
-    private VBox VboxAddMall;
-
-    @FXML
     private TextField MallName;
 
     @FXML
@@ -50,32 +52,61 @@ public class MainStageController implements  Initializable{
     @FXML
     private TextField MallFloors;
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         TableName.setCellValueFactory(new PropertyValueFactory<>("Name"));
         TableAdress.setCellValueFactory(new PropertyValueFactory<>("Adress"));
         TableFloors.setCellValueFactory(new PropertyValueFactory<>("Floors"));
-        for (Mall m : MallService.GetRepository().find()) {
-            list.add(m);
-        }
-
-        malluri.addAll(list);
-        Table.getItems().addAll();
-        Table.setItems(malluri);
+        Table.setItems(getMalls());
     }
 
     private ObservableList<Mall> malluri = FXCollections.observableArrayList();
-
     private ArrayList<Mall> list = new ArrayList<>();
 
-    public void handleLoginAction()  {
+    private ObservableList<Mall> getMalls()  {
+        for (Mall ma : MallService.GetRepository().find())
+            list.add(ma);
+
+        malluri.addAll(list);
+        return malluri;
+    }
+
+    public void handleAddAction() {
+
+        Mall m = new Mall();
+        if(MallName.getText().equals("") || MallAdress.getText().equals("") || MallFloors.getText().equals("")){
+            AddException.displayInvalid();
+            return;
+        }
+        m.setName(MallName.getText());
+        m.setAdress(MallAdress.getText());
+        m.setFloors(MallFloors.getText());
+
         try {
-            MallService.addMall(MallName.getText(), MallAdress.getText(), MallFloors.getText());
-        } catch (UsernameAlreadyExistsException e) {
+            checkMallDoesNotAlreadyExist(MallName.getText());
+            MallService.GetRepository().insert(m);
+            Table.getItems().add(m);
+        }catch (UsernameAlreadyExistsException e){
             AddException.displayInvalid();
         }
+
+        MallName.clear();
+        MallAdress.clear();
+        MallFloors.clear();
+    }
+    public void handleActionDelete(){
+
+        ObservableList<Mall> MallSelected , AllMalls;
+
+        AllMalls = Table.getItems();
+        MallSelected = Table.getSelectionModel().getSelectedItems();
+
+        for(Mall i : MallSelected)
+        {
+            MallService.GetRepository().remove(i);
+        }
+
+        MallSelected.forEach(AllMalls::remove);
     }
 
     public void AddMall(){
