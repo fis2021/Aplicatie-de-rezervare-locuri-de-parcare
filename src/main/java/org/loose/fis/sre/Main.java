3cd5a6488bd64ca774d51dbb55b28a3cd7cd0586
Roncea Marius-Alexandru <marius.roncea@student.upt.ro>
2021-05-16 15:10:31 +0300

    Am adaugat functionalitatea ca un user sa ocupe un loc de parcare (butonul se face rosu).

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -3,11 +3,9 @@ package org.loose.fis.sre;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
+import javafx.scene.image.Image;
 import javafx.stage.*;
-import org.loose.fis.sre.services.FileSystemService;
-import org.loose.fis.sre.services.MallService;
-import org.loose.fis.sre.services.PriceService;
-import org.loose.fis.sre.services.UserService;
+import org.loose.fis.sre.services.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -22,6 +20,8 @@ public class Main extends Application {
         PriceService.initDatabasePrice();
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Register.fxml"));
+
+        primaryStage.getIcons().add(new Image("images/ParkingIcon.png"));
         primaryStage.setTitle("Aplicatie de rezervare locuri de parcare");
         primaryStage.setScene(new Scene(root, 770, 532));
         primaryStage.show();
--- src/main/java/org/loose/fis/sre/controllers/MainStageController.java
+++ src/main/java/org/loose/fis/sre/controllers/MainStageController.java
@@ -49,12 +49,6 @@ public class MainStageController implements  Initializable{
     private HBox Mall2;
 
     @FXML
-    private Button a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18;
-
-    @FXML
-    private Button c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18;
-
-    @FXML
     private TableView<Mall> Table = new TableView<>();
 
     @FXML
@@ -98,6 +92,9 @@ public class MainStageController implements  Initializable{
 
     @FXML
     private TextField Price2;
+
+    @FXML
+    private Button SignOutAdmin;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -276,6 +273,12 @@ public class MainStageController implements  Initializable{
         Mall1.setVisible(false);
         Mall2.setVisible(false);
         VboxPrice.setVisible(true);
+    }
+
+    public void SignOut()
+    {
+        Stage stage = (Stage) SignOutAdmin.getScene().getWindow();
+        stage.close();
     }
 
     public static void display() {
